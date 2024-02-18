6fe984f23d059b23815b254f5018b7ee888f0b0c
andre <andrinho@gmail.com>
2024-02-18 06:01:54 -0300

    completed versao alpha do quiz

---
+++ quiz/README.md
@@ -0,0 +1,20 @@
+## Quiz feito para ajudar minha filha no interesse pela leitura.
+
+### Feito com linguagem bem simples com temas dos desenhos que ela mais gosta.
+
+Folder guarda todas as perguntas e respostas
+Dentro do folder subfolders com o nome do desenho especifico, nesse folder residem 2 arquivos de texto; perguntas e as respostas
+O jogo começa com o jogador podendo decidir qual o tema do quiz
+PJ Masks
+Frozen
+Morphle
+
+O jogo então carrega o arquivo com as perguntas referentes ao tema selecionado
+Aleatoriza a ordem das perguntas e a ordem das repostas
+
+Apresenta as questoes uma por uma
+O jogador responde o jogo avalia a resposta e
+Mensagem de resposta correta ou incorreta com a quantidade de pontos (pode ser estrelinhas)
+10 perguntas
+no fim o jogador recebe uma mensagem que o jogo acabou e seu resultado
+apos isso uma mensgam onde o jogador pode selecionar tentar novamente, mudar o tema ou fechar o jogo
---
+++ quiz/quiz.py
@@ -0,0 +1,134 @@
+import json
+import random
+
+QUIZ_DATA = 'data.json'
+
+# mensagem inicial
+def open_message():
+    print("***************************************")
+    print("***************************************")
+    print("***           Bem Vinda ao          ***")
+    print("***            Super quiz           ***")
+    print("***             MATILDA             ***")
+    print("***************************************")
+    print("***************************************")
+
+def load_temas(data):    
+    with open(data,'r') as dt:
+        dt = json.load(dt)
+        return dt['temas']
+
+def escolhe_tema(tms):
+    input_invalido = True
+    while input_invalido:
+        print("")
+        print("Escolha o assunto do Quiz: ")
+        # loop para criar as opçoes de tema baseado no json, acrescenta tb a caixinha com a letra associada para seleção
+        letras = ord('A')
+        for tm in tms:
+            print(f" ( {chr(letras)} ) {tm['name']}")
+            letras += 1
+        print("")
+        # guarda seleção do usuário e ja converte pra maiuscula para evitar conflitos de comparação
+        letra_selecionada = input("").upper()
+        print("")
+        # checa se a opcao selecionada é valida
+        if ( (len(letra_selecionada)>1) or ((ord(letra_selecionada) > ord('A')+len(tms)-1)) or (ord(letra_selecionada) < ord('A'))):
+            print(f"***   A opção {letra_selecionada} é invalida!    ***") 
+            print("")
+            continue
+        input_invalido = False
+    # verifica o tema selecionado e retorna seu valor
+    letras = ord('A')
+    for tm in tms:
+        if letra_selecionada == chr(letras):
+            return tm
+        letras+=1
+
+def fim_jogo():
+    print('fim de jogo')
+    print('pressione Q para sair')
+    print('pressione A para jogar de novo')
+    inp = input('')
+    if(inp.upper() == "A"):
+        start_game()
+
+def main_game(tema):
+
+    #nome do tema para display
+    name = tema['name']
+    #total de rodadas maxima do jogo
+    total_perguntas = len(tema['perguntas'])
+    #valor da resposta correta
+    valor = 10
+    #a estrelinha bonita
+    estrela = chr(9733)
+    
+    #contagem de perguntas que ja foram respondidas
+    respondidas_perguntas = 0
+    #acumula apos cada questao correta
+    pontuacao = 0
+    
+    #alimentamos duas listas com todas as perguntas e repostas referente o tema dessa partida
+    perguntas = []
+    respostas = []
+    for perg in tema['perguntas']:
+        perguntas.append(perg['texto'])
+        respostas.append(perg['respostas'])
+    
+    print(f"Vamos testar seu conhecimento em ### {name} ###!!")
+        
+    #loop da partida é aqui
+    while respondidas_perguntas < total_perguntas:
+        
+        #escolhe um numer aleatorio para ser escolhido na lista de perguntas
+        ale = random.randrange(0,len(perguntas))
+
+        #com o numero aleatorio, seleciona a pergunta, item deve ser removido apos utilizar
+        pergunta = perguntas[ale]
+        
+
+        #printa a pergunta da rodada
+        print(f'{respondidas_perguntas+1}) {pergunta}')
+
+        #salva a reposta correta antes de randomizar a ordem das respostas
+        resposta_correta = respostas[ale][0].upper()
+        print(f"a resposta correta é: {resposta_correta}")
+
+        #randomiza a ordem que as respostas vao ser displayed
+        random.shuffle(respostas[ale])
+
+        #display as respostas
+        for resp in respostas[ale]:
+            print(f"--> {resp}")
+        
+        #resposta do usuario
+        resposta_player = input("Escreva sua resposta: ").upper()
+
+        #se resposta for correta ganha pontos e uma mensagem de parabens
+        if (resposta_player == resposta_correta):
+            pontuacao += 1
+            print(f'Acertouuuu!!!!!')
+            print(f'Voce tem um total de {pontuacao} estrela(s)!') 
+            print(f'{estrela*pontuacao}')
+        else:
+            print('voce errou')
+        
+        perguntas.pop(ale)
+        respostas.pop(ale)
+        respondidas_perguntas +=1
+
+        if respondidas_perguntas == total_perguntas:
+            pass
+
+    fim_jogo()    
+
+def start_game():
+    open_message()
+    temas = load_temas(QUIZ_DATA)
+    tema = escolhe_tema(temas)
+    
+    main_game(tema)
+
+start_game()
+
