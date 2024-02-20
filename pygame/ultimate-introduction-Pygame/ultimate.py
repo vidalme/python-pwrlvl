d15b4a4837b1f6b97dddf0f9ca35619dd68db761
andre <andrinho@gmail.com>
2024-02-20 05:17:27 -0300

    mais uma bateria de pygame ultimate tuto

--- pygame/ultimate-introduction-Pygame/ultimate.py
+++ pygame/ultimate-introduction-Pygame/ultimate.py
@@ -5,17 +5,48 @@ from sys import exit
 def display_score():
     current_time = round((pygame.time.get_ticks() - start_time)/1000)
     score_surface = score_font.render(f'Score: {current_time}',False, (64,64,64))
+    
     score_rect = score_surface.get_rect(center=(400,50))
     screen.blit(score_surface,score_rect)
-    
 
+    global recorde
+    if (recorde <= current_time):
+        recorde = current_time
+
+
+def display_init_page():
+    title_surface = title_font.render(f'Pula Filha!', False, (111,196,169))
+    title_rect = title_surface.get_rect(center=(400,70))
+    global recorde
+    recorde_surface  = recorde_font.render(f'melhor score [ {recorde} ]',False,(55,77,96))
+    recorde_rect = recorde_surface.get_rect(center=(400, 300))
+
+    menu_surface = menu_font.render(f'[ espaco ] inicia jogo      [ S ] sai do jogo',False,'Black')
+    menu_rect = menu_surface.get_rect(center=(400,355))
+
+    guide_surface = guide_font.render(f'Aperte espaco ou clique com o mouse no personagem para pular e desviar dos inimigos.', False, (55,77,96))
+    guide_rect = guide_surface.get_rect(center=(400,380))
+
+    screen.blit(title_surface, title_rect)
+    screen.blit(recorde_surface,recorde_rect)
+    screen.blit(guide_surface, guide_rect)
+    screen.blit(menu_surface, menu_rect)
+    
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+pygame.display.set_caption('Pula Filha!')
 clock = pygame.time.Clock()
+
 score_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+title_font = pygame.font.Font('font/Pixeltype.ttf', 100)
+recorde_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+menu_font = pygame.font.Font('font/Pixeltype.ttf', 35)
+guide_font = pygame.font.Font('font/Pixeltype.ttf', 25)
+
 game_active = False
 start_time = 0
+recorde = 0
+
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -34,9 +65,16 @@ player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alp
 player_stand = pygame.transform.rotozoom(player_stand,0,2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
+#title 
+title_surface = title_font.render(f'Runner', False, ('Green'))
+title_rect = title_surface.get_rect(center=(400,20))
+
+
 # score_surface = score_font.render("My game", False, (64,64,64) )
 # score_rect = score_surface.get_rect(center=(400,50))
 #screen.fill('red')
+
+
 
 while True:
     
@@ -74,7 +112,6 @@ while True:
 
         # pygame.draw.rect(screen,'#c0e8ec',score_rect)
         # pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-        
 
         display_score()
 
@@ -100,7 +137,8 @@ while True:
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand, player_stand_rect)
-
+        display_init_page()
+        
     # keys = pygame.key.get_pressed()
     # if keys[pygame.K_SPACE]:
     #     print('jump')
